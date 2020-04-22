d58ce5c3cabd5a3d4e383c7845f5dff2b4c074fd
Ling Lan <llan@redhat.com>
2020-04-22 11:11:39 -0500

    add Git credential config in before_script

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -3,8 +3,4 @@ echo "BUILD GOES HERE!"
 
 echo "<repo>/<component>:<tag> : $1"
 
-git config --global credential.helper store
-echo "https://${GITHUB_TOKEN}:x-oauth-basic@github.com" >> ~/.git-credentials
-export GOPRIVATE=github.com/open-cluster-management
-
 operator-sdk build $1
