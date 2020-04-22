10aa11f283772c001940a5bc777a2e5638ffba8a
Ling Lan <llan@redhat.com>
2020-04-21 20:18:05 -0500

    try git credential helper

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -3,6 +3,8 @@ echo "BUILD GOES HERE!"
 
 echo "<repo>/<component>:<tag> : $1"
 
+git config --global credential.helper store
+echo "https://${GITHUB_TOKEN}:x-oauth-basic@github.com" >> ~/.git-credentials
 export GOPRIVATE=github.com/open-cluster-management
 
 operator-sdk build $1
