777238bc842abcc403cb45f6856647118609c5c3
Song Song Li <ssli@redhat.com>
2020-06-18 18:47:21 +0800

    add label and lic

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Copyright (c) 2020 Red Hat, Inc.
 
-echo "<repo>/<component>:<tag> : $1"
+set -e
 
-operator-sdk build $1
+make docker-binary
+
+echo "Building endpoint-monitoring-operator image"
+export DOCKER_IMAGE_AND_TAG=${1}
+export DOCKER_FILE=Dockerfile
+make docker/build
