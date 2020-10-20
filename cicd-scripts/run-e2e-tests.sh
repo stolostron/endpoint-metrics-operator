1f39903bcf631c02a795f8fa9d5728949e6bf393
Ling(Marco) Lan <llan@redhat.com>
2020-10-20 08:07:11 -0500

    add the setup in e2e using observability-kind-cluster (#52)

--- cicd-scripts/run-e2e-tests.sh
+++ cicd-scripts/run-e2e-tests.sh
@@ -4,3 +4,13 @@
 echo "<repo>/<component>:<tag> : $1"
 
 git config --global url."https://$GITHUB_TOKEN@github.com/open-cluster-management".insteadOf "https://github.com/open-cluster-management"
+
+WORKDIR=`pwd`
+cd ${WORKDIR}/..
+git clone https://github.com/open-cluster-management/observability-kind-cluster.git
+cd observability-kind-cluster
+./setup.sh $1
+if [ $? -ne 0 ]; then
+    echo "Cannot setup environment successfully."
+    exit 1
+fi
