d59430ff9db239223c01104bf680fa596b3043a5
Marco <llan@redhat.com>
2020-11-13 23:35:20 -0600

    watch all local ns

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -86,7 +86,7 @@ func main() {
 
 	// Set default manager options
 	options := manager.Options{
-		Namespace:          namespace,
+		Namespace:          "",
 		MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
 	}
 
