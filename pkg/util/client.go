14fd80746f4ecfafc68da626a70fecb15738e62a
Marco <llan@redhat.com>
2021-02-19 01:49:22 -0600

    add observabilityaddon into client scheme

--- pkg/util/client.go
+++ pkg/util/client.go
@@ -3,9 +3,12 @@
 package util
 
 import (
+	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
+
+	"github.com/open-cluster-management/multicluster-monitoring-operator/pkg/apis"
 )
 
 const (
@@ -24,8 +27,14 @@ func CreateHubClient() (client.Client, error) {
 		return nil, err
 	}
 
-	// generate the client based on the config
-	hubClient, err := client.New(config, client.Options{})
+	s := scheme.Scheme
+	if err := apis.AddToScheme(s); err != nil {
+		return nil, err
+	}
+
+	// generate the client based off of the config
+	hubClient, err := client.New(config, client.Options{Scheme: s})
+
 	if err != nil {
 		log.Error(err, "Failed to create hub client")
 		return nil, err
