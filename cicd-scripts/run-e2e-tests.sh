3c57208c05c267da2cdd3a5cae358b06daa3ab46
morvencao <lcao@redhat.com>
2021-03-16 03:29:53 +0000

    enable e2e test with prow and remove travis.

--- cicd-scripts/run-e2e-tests.sh
+++ cicd-scripts/run-e2e-tests.sh
@@ -1,47 +1,8 @@
 #!/bin/bash
 # Copyright (c) 2021 Red Hat, Inc.
 # Copyright Contributors to the Open Cluster Management project.
-echo "<repo>/<component>:<tag> : $1"
 
-git config --global url."https://$GITHUB_TOKEN@github.com/open-cluster-management".insteadOf "https://github.com/open-cluster-management"
-
-WORKDIR=`pwd`
-cd ${WORKDIR}/..
-git clone https://github.com/open-cluster-management/observability-kind-cluster.git
-cd observability-kind-cluster
-./setup.sh $1
-if [ $? -ne 0 ]; then
-    echo "Cannot setup environment successfully."
-    exit 1
-fi
-
-cd ${WORKDIR}/..
-git clone https://github.com/open-cluster-management/observability-e2e-test.git
+git clone --depth 1 https://github.com/open-cluster-management/observability-e2e-test.git
 cd observability-e2e-test
+make test-e2e
 
-go get -u github.com/onsi/ginkgo/ginkgo
-
-export KUBECONFIG=$HOME/.kube/kind-config-hub
-export SKIP_INSTALL_STEP=true
-
-git clone https://github.com/open-cluster-management/observability-gitops.git
-
-printf "options:" >> resources/options.yaml
-printf "\n  hub:" >> resources/options.yaml
-printf "\n    baseDomain: placeholder" >> resources/options.yaml
-printf "\n    masterURL: https://127.0.0.1:32806" >> resources/options.yaml
-printf "\n    grafanaURL: http://127.0.0.1" >> resources/options.yaml
-printf "\n    grafanaHost: grafana-test" >> resources/options.yaml
-printf "\n  clusters:" >> resources/options.yaml
-printf "\n    - name: spoke" >> resources/options.yaml
-printf "\n      masterURL: https://127.0.0.1:32807" >> resources/options.yaml
-printf "\n      kubeconfig: $HOME/.kube/kind-config-spoke" >> resources/options.yaml
-
-ginkgo -v ./pkg/tests -- -options=../../resources/options.yaml
-
-cat ./pkg/tests/results.xml | grep failures=\"0\" | grep errors=\"0\"
-if [ $? -ne 0 ]; then
-    echo "Cannot pass all test cases."
-    cat results.xml
-    exit 1
-fi
