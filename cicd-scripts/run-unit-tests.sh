b506cd27a1db04b6d161bcbf8069355241417b7a
morvencao <lcao@redhat.com>
2022-01-11 16:11:45 +0800

    rename org.
    Signed-off-by: morvencao <lcao@redhat.com>

--- cicd-scripts/run-unit-tests.sh
+++ cicd-scripts/run-unit-tests.sh
@@ -3,6 +3,4 @@
 # Copyright Contributors to the Open Cluster Management project.
 echo "<repo>/<component>:<tag> : $1"
 
-git config --global url."https://$GITHUB_TOKEN@github.com/open-cluster-management".insteadOf "https://github.com/open-cluster-management"
-
 go test ./...
